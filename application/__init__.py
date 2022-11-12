4919b89958b0f3201b777775d357704c196b55b9
UpwardRow <downeyadam0312@gmail.com>
2022-11-12 14:58:12 +0000

    Added forms.py with Related Functionality
    I created the forms.py file to the project. I created this on a branch
    called forms-file-work which was a bad idea, since I did more work than
    that. routes.py and __init__.py was edited to introduce validators.
    .gitignore was also edited.

--- application/__init__.py
+++ application/__init__.py
@@ -5,10 +5,14 @@ import os
 # Application definition
 app = Flask(__name__)
 
+# Creating a bytes object which contains random bytes for use of Flask secret key
+SECRET_KEY = os.urandom(1000)
+
 # Configuration variables
 # Connection string to connect to database with an environmental variables
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
 
 # Database class
 db = SQLAlchemy(app)
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,19 @@
 from application import app
-# from application.models import Subjects, Staff
-from flask import render_template
-# from flask import request
-# from application.forms import NameForm, exampleSelectField, SubjectList
+from application.models import Customer, Ticket
+from flask import render_template, request
+from application.forms import CustomerCredentials
 
 
-@app.route('/home')
+@app.route('/home', methods=['GET', 'POST'])
 def home():
+    # Instantiated object of class UserCredentials
+    login_form = CustomerCredentials()
+
+    if login_form.validate_on_submit():
+        if request.method == 'POST':
+            check_customer = Customer(customer_username=login_form.username.data,
+                                      customer_password=login_form.password.data)
+
+            return "I did something?"
+
     return render_template("homepage.html")
