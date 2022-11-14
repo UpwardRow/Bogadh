b6c6614c6e8893188ca4b5274b9d2794ebc49c32
UpwardRow <downeyadam0312@gmail.com>
2022-11-14 16:40:52 +0000

    Homepage Functionality Complete
    The homepage functionality has been complete. A user may give a username
    and a password, then they will be brought to the tickets_view.html page.
    There are customer validators implemented, along with a template
    additions in the commit too.

--- application/forms.py
+++ application/forms.py
@@ -1,18 +1,47 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, ValidationError
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, Length
 from application.models import Customer
 
 
+# Class is being used to validate the user information being put into the form
+class CustomerCredentialsCheck:
+    def __init__(self, message=None):
+        if not message:
+            message = "The password is incorrect"
+        self.message = message
+
+    def __call__(self, form, field, auto_invoke=False):
+
+        # This statement is a check to verify if the data entered into either username or password field is equal to db
+        if field == form.username:
+            other_field = form.password
+
+            password_object = Customer.query.filter_by(password=other_field.data).first()
+            username_object = Customer.query.filter_by(customer_username=field.data).first()
+        else:
+            other_field = form.username
+
+            password_object = Customer.query.filter_by(password=field.data).first()
+            username_object = Customer.query.filter_by(customer_username=other_field.data).first()
+
+        # If the password or username is not found in the database, a validation error will be raised
+        if password_object is None or username_object is None:
+            raise ValidationError(self.message)
+        return True
+
+
 class CustomerCredentials(FlaskForm):
-    username = StringField("Username", validators=[InputRequired("Username required")])
-    password = StringField("Password", validators=[InputRequired("Password required")])
+    # WTForms passes the value inputted into the field box as a default parameter 'field', no parameter value needed
+    username = StringField("Username", validators=[InputRequired("Username required"),
+                                                   CustomerCredentialsCheck(message="Username not found")
+                                                   ])
+
+    password = StringField("Password", validators=[InputRequired("Password required"),
+                                                   CustomerCredentialsCheck(message="Incorrect password")
+                                                   ])
     submit = SubmitField("Login")
 
-    def validate_username(self, username):
-        username_object = Customer.query.filter_by(username=username.data).first()
-        if not username_object:
-            raise ValidationError("Username was not found")
 
 # class Ticket(FlaskForm):
 #     destination_address =
--- application/routes.py
+++ application/routes.py
@@ -1,19 +1,27 @@
 from application import app
-from application.models import Customer, Ticket
-from flask import render_template, request
+from flask import render_template, request, redirect
 from application.forms import CustomerCredentials
 
 
-@app.route('/home', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
     # Instantiated object of class UserCredentials
     login_form = CustomerCredentials()
 
+    # This will check if the request is a GET, meaning that the page is just being opened
+    if request.method == "GET":
+        return render_template("homepage.html", title="home", form=login_form)
+
+    # These statements check if the username matches to one in the database. If so, the user will be taken to tickets
     if login_form.validate_on_submit():
-        if request.method == 'POST':
-            check_customer = Customer(customer_username=login_form.username.data,
-                                      customer_password=login_form.password.data)
+        username = login_form.username.data
+        password = login_form.password.data
 
-            return "I did something?"
+        return render_template("tickets_view.html", title="home", form=login_form, username=username, password=password)
+    else:
+        return render_template("homepage.html", title="home", form=login_form, username="", password="")
 
-    return render_template("homepage.html")
+
+@app.route('/tickets_view.html', methods=['GET', 'POST'])
+def tickets_view():
+    return render_template("tickets_view.html", title="tickets_view")
