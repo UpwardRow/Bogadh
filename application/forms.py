0cbc71ba09db00545a5b250d64b75573fb634313
UpwardRow <downeyadam0312@gmail.com>
2022-11-16 02:00:04 +0000

    Ticket Addition
    This is probably the largest commit of the project. I added Ticket
    functionality to allow a user to insert data into a form to be pushed to
    the database. This is partly works, but database cardinality stopped full implementation

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, ValidationError
-from wtforms.validators import InputRequired, Length
-from application.models import Customer
+from wtforms import StringField, SubmitField, ValidationError, IntegerField, DateTimeField
+from wtforms.validators import InputRequired
+from application.models import Customer, Ticket
 
 
 # Class is being used to validate the user information being put into the form
@@ -31,6 +31,23 @@ class CustomerCredentialsCheck:
         return True
 
 
+# A validator to make sure that the stage is in the expected range
+class StageLimitCheck:
+    def __init__(self, message=None):
+        if not message:
+            message = "Stage must not be above 3, and must be above 0"
+        self.message = message
+
+    def __call__(self, form, field):
+
+        stage_object = Ticket.query.filter_by(stage=field.data).first()
+
+        # If the number for stage is above 3 or below 1, a ValidationError will be raised
+        if stage_object > 3 or stage_object < 1:
+            raise ValidationError(self.message)
+        return True
+
+
 class CustomerCredentials(FlaskForm):
     # WTForms passes the value inputted into the field box as a default parameter 'field', no parameter value needed
     username = StringField("Username", validators=[InputRequired("Username required"),
@@ -43,6 +60,15 @@ class CustomerCredentials(FlaskForm):
     submit = SubmitField("Login")
 
 
-# class Ticket(FlaskForm):
-#     destination_address =
-#     stage =
+# def destination_query():
+#     return Route.query
+
+class TicketsBuy(FlaskForm):
+    destination = StringField("Destination", validators=[InputRequired("Destination required")])
+
+    comment = StringField("Comment")
+
+    stage = IntegerField("Stage", validators=[StageLimitCheck("The stage is not between 0-4")])
+
+    departure_time_and_date = DateTimeField("departure_time_and_date", format='%Y-%m-%d %H:%M:%S',
+                                            validators=[InputRequired()])
